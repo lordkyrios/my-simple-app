c0f5b7a6ab1dc17757d68a4d5c35ab05bbff1601
Lord Kyrios <lordkyrios@s506981@gmail.com>
2025-07-19 19:26:47 -0500

    Fix: added missing dependency and ESLint config

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,8 @@
 // lib/utils.ts
-export function cn(...classes: (string | false | null | undefined)[]): string {
-  return classes.filter(Boolean).join(" ");
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: any[]) {
+  return twMerge(clsx(inputs));
 }
+
