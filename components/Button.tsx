4693bcf9fa9581e285aa5cefb3b0efc6595bab5c
lordkyrios <lbeecher015@gmail.com>
2025-07-22 21:34:16 -0500

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,6 @@
-// components/Button.tsx
-import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "../lib/utils";
 import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import clsx from "clsx";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-xl text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
@@ -31,17 +30,15 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {}
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, ...props }, ref) => {
     return (
       <button
-        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        className={clsx(buttonVariants({ variant, size }), className)}
         {...props}
       />
     );
   }
 );
 Button.displayName = "Button";
-
-export { Button, buttonVariants };
